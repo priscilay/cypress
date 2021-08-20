47ad0ec37f8e528bd102373af8cfffefc1a45ac0
Priscila <priscila_franca1993@hotmail.com>
2021-08-20 12:22:38 -0300

    continuacao hooks

--- cypress/integration/basics.spec.js
+++ cypress/integration/basics.spec.js
@@ -1,16 +1,33 @@
 /// <reference types="cypress" />
 
 describe('Cypress basics', () =>{
-    it('Shoud visit a page and assert tittle', () => {
+    it.only('Shoud visit a page and assert tittle', () => {
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
-        //https://wcaquino.me/cypress/componentes.html
 
         // const title = cy.title()
         // console.log(title)
+        cy.pause()
+
+        cy.title().should('be.equal', 'Campo de Treinamento')
+        //cy.title().debug().should('contain', 'Campo')
 
         cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
+            //todo imprimir o log no console
+            //escrever um campo de texto
+
+    })
+
+    it('Interagindo com elementos', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+        cy.get('#buttonSimple')
+            .click()
+            .should('have.value', 'Obrigado!')
+    })
+})  
 
 
-        })
 
-})
+
---
+++ cypress/integration/elements.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe('Work with basic elements', () => {
+    beforeEach(() => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+    
+    it('Texts', () => {
+        cy.get('body').should('contain', 'Cuidado')
+        // cy.get('body').should('have.text', 'Cuidado')
+        cy.get('span').should('contain', 'Cuidado')
+        // cy.get('div').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('contain', 'Cuidado')
+        cy.get('.facilAchar').should('have.text', 'Cuidado onde clica, muitas armadilhas...')
+    })
+
+    it('Links', () => {
+        // cy.get('[href="./frame.html"]').click()
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text','Voltou!')
+
+        cy.reload()
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        cy.contains('Voltar').click()
+        cy.get('#resultado').should('have.text','Voltou!')
+
+    })
+
+
+})
